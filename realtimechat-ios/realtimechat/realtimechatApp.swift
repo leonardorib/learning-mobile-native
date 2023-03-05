38ef430c52b3adf1092bfe58c11f6d6e6fc6ac28
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2023-03-05 18:50:58 -0300

    Showing MainMessages so testing becomes easier

--- realtimechat-ios/realtimechat/realtimechatApp.swift
+++ realtimechat-ios/realtimechat/realtimechatApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct realtimechatApp: App {
     var body: some Scene {
         WindowGroup {
-            LoginAndRegisterView()
+            MainMessagesView()
         }
     }
 }
